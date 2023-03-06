ef2678d8cf28869732413d77658d0b402e5e004a
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2023-03-06 10:55:47 +0200

    Add basic styles

---
+++ src/components/Generator/Generator.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Generator.module.scss'
+
+export default function Generator() {
+  return (
+    <main>
+      <section className={styles.card}>
+        <h2>Advice</h2>
+        <h1>Full advice</h1>
+      </section>
+    </main>
+  )
+}
